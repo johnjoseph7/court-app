d557d509de9fe6eaf78428dc272cca487949a84a
Johnson J <93112488+johnjoseph7@users.noreply.github.com>
2024-03-23 10:37:13 +0530

    Update MyCase.jsx

--- src/components/MyCase.jsx
+++ src/components/MyCase.jsx
@@ -1,6 +1,5 @@
-import {React,useState} from "react";
+import {React} from "react";
 import { Link } from "react-router-dom";
-import { SideBar } from "./SideBar";
 
 export const MyCase = () => {
   return (
